2a4128a7b877f3892c9a2383da402522116f18cc
Thadeo Arlo <thadeoarlo@gmail.com>
2017-12-24 14:12:24 +0700

    added SimpleMotionSensor

--- MotionActivatedSwitch.ino
+++ MotionActivatedSwitch.ino
@@ -1,9 +1,9 @@
 //Simple Arduino control with PIR sensor
 
-int outPin = 13;
+int outPin = 13; //defineing the pins
 int inPin = 2;
-int pirState = LOW;
-int val = 0;
+int pirState = LOW; //pir initial state
+int pirValue = digitalRead(inPin);
 
 void setup() {
     pinMode(outPin, OUTPUT);
@@ -12,10 +12,10 @@ void setup() {
 }
 
 void loop() {
-  val = digitalRead(inPin);
-  if (val == HIGH) {
+  
+  if (pirValue == HIGH) {
     digitalWrite(outPin, HIGH);
-    delay(120000); // 60000ms = 1 minute
+    delay(120000); // 60000ms = 1 minute, set how long you want the output to be.
 
     if (pirState == LOW) {
       Serial.println("Motion Detected!");
